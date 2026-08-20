4234da4bcc6c04bfba4c6450a2d4dd336f96e166
Nsengumuremyi Odillon <166166466+odlo123@users.noreply.github.com>
2026-08-20 15:45:35 +0200

    Change Brevo API key in testmail.py
    Updated Brevo API key for email sending.

--- testmail.py
+++ testmail.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 BREVO_API_URL ="https://api.brevo.com/v3/smtp/email"
 
-BREVO_API_KEY = "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"
+BREVO_API_KEY = "xsmtpsib-48acbef24463246121e533bd0de661f71923c8390ead31912c6bff9d625530bf-x7M5a3hylV8n7aO7"
 BREVO_SENDER_EMAIL = "hahishapp@gmail.com"
 BREVO_SENDER_NAME = os.getenv(
     "BREVO_SENDER_NAME",
